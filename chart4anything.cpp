5b7dfc89ed1ab1652f0f212d42167f1637f0d9d9
Vaskka <1139851358@qq.com>
2019-11-26 15:23:27 +0800

    clean project

--- chart4anything.cpp
+++
@@ -1,9 +0,0 @@
-#include "chart4anything.h"
-
-
-
-Chart4Anything::Chart4Anything(QQmlEngine &engine) : ChartObject("Chart4Anything", nullptr)
-{
-    this->pipline = new Pipline(this);
-    this->view = new View(engine, this);
-}
--- core/chart4anything.cpp
+++
@@ -1,6 +0,0 @@
-#include "chart4anything.h"
-
-Chart4Anything::Chart4Anything(QObject *parent) : QObject(parent)
-{
-
-}
